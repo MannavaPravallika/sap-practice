bb8307364f30eed82c0c2cba1294aff6a3cdf67e
MannavaPravallika <147588971+MannavaPravallika@users.noreply.github.com>
2023-10-12 16:47:52 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,27 @@
+const fruit1="apple";
+const fruit2="mango";
+const fruit3="cherry";
+const fruit4="orange";
+const fruit5="banana";
+
+const fruitList=[
+"apple",
+"mango",
+"cherry",
+"orange",
+"banana",
+5,
+"undifined",
+"null",
+"true",
+"false",
+]
+console.log(fruitList);
+//console.log(fruitList[0]);
+
+//let bestFruit=fruitList[4];
+//console.log(bestFruit);
+
+fruitList[5]="grapes"
+console.log(fruitList);
+
